fc132c982c05dd42e52147b5cc265976ef6f487c
Ben Adam Rosenberg <ben.rosenberg@gmail.com>
2017-06-08 14:42:16 -0700

    build delegate dataset

--- scripts/build-data.js
+++ scripts/build-data.js
@@ -28,13 +28,6 @@ function candidatesFromGS(position, candidateData, nominationData) {
                 qualifications: item["whyAreYouQualifiedForThisSpecificRole?"],
                 goals: item["whatDoYouHopeToAccomplishForTheChapterIfElectedToThisRole?"],
 		nominations: nominations,
-		/*
-                nominiation1Name: fakeData.name(),
-                nominiation2Name: fakeData.name(),
-                nominiation3Name: fakeData.name(),
-                nominiation4Name: fakeData.name(),
-                nominiation5Name: fakeData.name()
-		*/
             });
         }
     }
@@ -42,32 +35,64 @@ function candidatesFromGS(position, candidateData, nominationData) {
     return candidates;
 }
 
+function delegatesFromGS(delegateData, delegateNominationData) {
+    var delegates = [];
+
+    for(var i in delegateData) {
+        var item = delegateData[i];
+	    var nominations = [];
+	    for(var j in delegateNominationData) {
+		var nomination = delegateNominationData[j];
+		if(item["what'sYourName?"] == nomination["whoAreYouNominating?"]) {
+		    nominations.push({
+			name: nomination["yourName"],
+		    });
+
+	        }
+	    }
+            delegates.push({
+                name: item["what'sYourName?"],
+                slug: candidateSlug("Delegate", item["what'sYourName?"]),
+                imageUrl: fakeData.image(),
+                reason: item["whyDoYouWantToRepresentYourComradesAtTheDsaNationalConvention?"],
+		nominations: nominations,
+            });
+    }
+
+    return delegates;
+}
+
 gsjson({
   spreadsheetId: process.env.LOCAL_COUNCIL_ELECTION_SHEET_ID
 }).then(function(gsCandidateJson) {
-  //console.log(gsCandidateJson.length);
-  //console.log(gsCandidateJson);
   // I'm sorry
   // !!! TODO: make this elegant
   gsjson({
     spreadsheetId: process.env.NOMINATIONS_SHEET_ID
-    }).then(function(gsNominationsJson) {
-	//console.log(gsNominationsJson);
-        const data = serialize({
-            localCouncil: {
-              "Co-chair": candidatesFromGS("Co-chair", gsCandidateJson, gsNominationsJson ),
-	      "Vice Chair": candidatesFromGS("Vice Chair", gsCandidateJson, gsNominationsJson),
-	      "Secretary": candidatesFromGS("Secretary", gsCandidateJson, gsNominationsJson),
-	      "Treasurer": candidatesFromGS("Treasurer", gsCandidateJson, gsNominationsJson),
-	      "Internal Organizer": candidatesFromGS("Internal Organizer", gsCandidateJson, gsNominationsJson),
-	      "External Organizer": candidatesFromGS("External Organizer", gsCandidateJson, gsNominationsJson),
-	      "At Large Member of Local Council": candidatesFromGS("At Large Member of Local Council", gsCandidateJson, gsNominationsJson),
-        },
-        nationalConvention: {
-          candidates: Array(30).fill().map(fakeData.candidate)
-        }
-      });
-	//console.log(data);
-	jsonfile.writeFileSync('./src/data/election-data.json', data);
-    });
+  }).then(function(gsNominationsJson) {
+	gsjson({
+	spreadsheetId: process.env.NATIONAL_CONVENTION_ELECTION_SHEET_ID
+	}).then(function(gsDelegatesJson) {
+		gsjson({
+		    spreadsheetId: process.env.CONVENTION_NOMINATIONS_SHEET_ID
+		}).then(function(gsDelegateNominationsJson) {
+			const data = serialize({
+				localCouncil: {
+				"Co-chair": candidatesFromGS("Co-chair", gsCandidateJson, gsNominationsJson ),
+				"Vice Chair": candidatesFromGS("Vice Chair", gsCandidateJson, gsNominationsJson),
+				"Secretary": candidatesFromGS("Secretary", gsCandidateJson, gsNominationsJson),
+				"Treasurer": candidatesFromGS("Treasurer", gsCandidateJson, gsNominationsJson),
+				"Internal Organizer": candidatesFromGS("Internal Organizer", gsCandidateJson, gsNominationsJson),
+				"External Organizer": candidatesFromGS("External Organizer", gsCandidateJson, gsNominationsJson),
+				"At Large Member of Local Council": candidatesFromGS("At Large Member of Local Council", gsCandidateJson, gsNominationsJson),
+				},
+				nationalConvention: {
+					candidates: delegatesFromGS(gsDelegatesJson, gsDelegateNominationsJson),
+				}
+			});
+
+		jsonfile.writeFileSync('./src/data/election-data.json', data);
+		});
+	});
+  });
 });
