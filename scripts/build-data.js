38a1ea72b3b26d4117691169e6b3221fcd76864d
Sean Durham <sean@sdurham.net>
2017-06-12 17:33:42 -0500

    Tidy up requests to google

--- scripts/build-data.js
+++ scripts/build-data.js
@@ -3,96 +3,76 @@ var gsjson = require('google-spreadsheet-to-json');
 const { serialize, candidateSlug } = require('../data/election-data');
 const fakeData = require('../data/fake-data');
 
-function candidatesFromGS(position, candidateData, nominationData) {
-    var candidates = [];
+function candidatesFromGS(position, candidatesData, nominationData) {
+  return candidatesData
+    .filter(c => c["whichPositionAreYouRunningFor?"] === position)
+    .map((item) => {
+      const nominations = nominationData
+        .filter(n => n["whoAreYouNominating?YouMayMakeOneNominationPerFormSubmission."] === item["firstAndLastName"])
+        .map((nomination) => {
+          return {
+            name: nomination["firstAndLastName"],
+            comment: nomination["optional:WhyDoYouThinkYourComradeIsRightForThisRole?"]
+          }
+        });
 
-    for(var i in candidateData) {
-        var item = candidateData[i];
-        if (item["whichPositionAreYouRunningFor?"] == position) {
-	    var nominations = [];
-	    for(var j in nominationData) {
-		var nomination = nominationData[j];
-		if(item["firstAndLastName"] == nomination["whoAreYouNominating?YouMayMakeOneNominationPerFormSubmission."]) {
-		    nominations.push({
-			name: nomination["firstAndLastName"],
-			comment: nomination["optional:WhyDoYouThinkYourComradeIsRightForThisRole?"]
-		    });
-
-	        }
-	    }
-            candidates.push({
-                name: item["firstAndLastName"],
-                slug: candidateSlug(item["firstAndLastName"]),
-                imageUrl: undefined,
-                involvement: item["pleaseDescribeYourInvolvementInEastBayDsa."],
-                qualifications: item["whyAreYouQualifiedForThisSpecificRole?"],
-                goals: item["whatDoYouHopeToAccomplishForTheChapterIfElectedToThisRole?"],
-		nominations: nominations,
-            });
-        }
-    }
-
-    return candidates;
+      return {
+        name: item["firstAndLastName"],
+        slug: candidateSlug(item["firstAndLastName"]),
+        imageUrl: undefined,
+        involvement: item["pleaseDescribeYourInvolvementInEastBayDsa."],
+        qualifications: item["whyAreYouQualifiedForThisSpecificRole?"],
+        goals: item["whatDoYouHopeToAccomplishForTheChapterIfElectedToThisRole?"],
+        nominations: nominations,
+      }
+    })
 }
 
-function delegatesFromGS(delegateData, delegateNominationData) {
-    var delegates = [];
+function delegatesFromGS(delegatesData, delegateNominationsData) {
+  return delegatesData
+    .map((delegate) => {
+      const nominations = delegateNominationsData
+        .filter(n => n["whoAreYouNominating?"] === delegate["what'sYourName?"])
+        .map((nomination) => {
+          return {
+            name: nomination["yourName"],
+          }
+        });
 
-    for(var i in delegateData) {
-        var item = delegateData[i];
-	    var nominations = [];
-	    for(var j in delegateNominationData) {
-		var nomination = delegateNominationData[j];
-		if(item["what'sYourName?"] == nomination["whoAreYouNominating?"]) {
-		    nominations.push({
-			name: nomination["yourName"],
-		    });
-
-	        }
-	    }
-            delegates.push({
-                name: item["what'sYourName?"],
-                slug: candidateSlug(item["what'sYourName?"]),
-                imageUrl: fakeData.image(),
-                reason: item["whyDoYouWantToRepresentYourComradesAtTheDsaNationalConvention?"],
-		nominations: nominations,
-            });
-    }
-
-    return delegates;
+      return {
+        name: delegate["what'sYourName?"],
+        reason: delegate["whyDoYouWantToRepresentYourComradesAtTheDsaNationalConvention?"],
+        nominations: nominations,
+      }
+    })
 }
 
-gsjson({
-  spreadsheetId: process.env.LOCAL_COUNCIL_ELECTION_SHEET_ID
-}).then(function(gsCandidateJson) {
-  // I'm sorry
-  // !!! TODO: make this elegant
-  gsjson({
-    spreadsheetId: process.env.NOMINATIONS_SHEET_ID
-  }).then(function(gsNominationsJson) {
-	gsjson({
-	spreadsheetId: process.env.NATIONAL_CONVENTION_ELECTION_SHEET_ID
-	}).then(function(gsDelegatesJson) {
-		gsjson({
-		    spreadsheetId: process.env.CONVENTION_NOMINATIONS_SHEET_ID
-		}).then(function(gsDelegateNominationsJson) {
-			const data = serialize({
-				localCouncil: {
-				"Co-chair": candidatesFromGS("Co-chair", gsCandidateJson, gsNominationsJson ),
-				"Vice Chair": candidatesFromGS("Vice Chair", gsCandidateJson, gsNominationsJson),
-				"Secretary": candidatesFromGS("Secretary", gsCandidateJson, gsNominationsJson),
-				"Treasurer": candidatesFromGS("Treasurer", gsCandidateJson, gsNominationsJson),
-				"Internal Organizer": candidatesFromGS("Internal Organizer", gsCandidateJson, gsNominationsJson),
-				"External Organizer": candidatesFromGS("External Organizer", gsCandidateJson, gsNominationsJson),
-				"At Large Member of Local Council": candidatesFromGS("At Large Member of Local Council", gsCandidateJson, gsNominationsJson),
-				},
-				nationalConvention: {
-					candidates: delegatesFromGS(gsDelegatesJson, gsDelegateNominationsJson),
-				}
-			});
+async function getElectionJson() {
+  try {
+    const candidates = await gsjson({ spreadsheetId: process.env.LOCAL_COUNCIL_ELECTION_SHEET_ID });
+    const candidateNominations = await gsjson({ spreadsheetId: process.env.NOMINATIONS_SHEET_ID });
+    const delegates = await gsjson({ spreadsheetId: process.env.NATIONAL_CONVENTION_ELECTION_SHEET_ID });
+    const delegateNominations = await gsjson({ spreadsheetId: process.env.CONVENTION_NOMINATIONS_SHEET_ID });
 
-		jsonfile.writeFileSync('./src/data/election-data.json', data);
-		});
-	});
-  });
-});
+    return { candidates, candidateNominations, delegates, delegateNominations };
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+getElectionJson().then((election) => {
+	jsonfile.writeFileSync('./src/data/election-data.json', serialize({
+		localCouncil: {
+      "Co-chair": candidatesFromGS("Co-chair", election.candidates, election.candidateNominations),
+      "Vice Chair": candidatesFromGS("Vice Chair", election.candidates, election.candidateNominations),
+      "Secretary": candidatesFromGS("Secretary", election.candidates, election.candidateNominations),
+      "Treasurer": candidatesFromGS("Treasurer", election.candidates, election.candidateNominations),
+      "Internal Organizer": candidatesFromGS("Internal Organizer", election.candidates, election.candidateNominations),
+      "External Organizer": candidatesFromGS("External Organizer", election.candidates, election.candidateNominations),
+      "At Large Member of Local Council": candidatesFromGS("At Large Member of Local Council", election.candidates, election.candidateNominations),
+		},
+		nationalConvention: {
+			candidates: delegatesFromGS(election.delegates, election.delegateNominations),
+		}
+	}));
+}).catch(e => { console.log(e) });
